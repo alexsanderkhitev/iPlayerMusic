8f6a43eeaa84f62a7b71c0a131141dca68107679
Александр Хитёв <Alexsander@Aleksandrs-MacBook-Pro.local>
2015-07-17 20:05:36 +0400

    Initial Commit

---
+++ Music/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Александр Хитёв on 7/17/15.
+//  Copyright (c) 2015 Alexsander Khitev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
